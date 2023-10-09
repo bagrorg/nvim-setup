baa8ce7c2645855699b9b395c31f4d02d4e383bc
bagrorg <kuldanvit@gmail.com>
2023-10-09 11:05:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+sudo apt install ripgrep python3.8-venv -y
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 cp nvim ~/.config/ -r
